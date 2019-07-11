4eee9d0a7fec0ecb397954ae78b68a753e5f3c3c
Agustina Sosa <agustinasf85@gmail.com>
2019-07-11 13:06:19 -0400

    added start script and made port dynamic

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+//make port dynamic by using .env
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
